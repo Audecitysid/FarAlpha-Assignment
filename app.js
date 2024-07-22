543712197a4e27ca6d768b56af9259e00f663fb6
Ubuntu <azureuser@challenge-siddhesh-dhomane.wemevqvv5vtedjrrpqrwhg5rhf.bx.internal.cloudapp.net>
2024-07-22 12:07:39 +0000

    Initial commit with backend API

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 80; 
+
+app.get('/sayHello', (req, res) => {
+  res.json({ message: 'Hello User' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+///home/azureuser/simple-api/app.js
