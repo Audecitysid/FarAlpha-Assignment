f23d7f76ddb2bbe92d848f176d36908c1abee58f
Ubuntu <ubuntu@ip-172-31-90-76.ec2.internal>
2024-11-21 14:49:22 +0000

    added blank

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 80; 
+const port = 5000; 
+
+app.get('/', (req, res) => {
+  res.send('<!DOCTYPE html><html lang="en"><head><title>Node Webserver</title></head><body><h1><center>Node Webserver Running</center></h1></body></html>');
+});
+
 
 app.get('/sayHello', (req, res) => {
   res.json({ message: 'Hello Users' });
